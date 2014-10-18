90f2385052bee9728371f631dbefff4ec949474f
Kory Petersen <kory.petersen23@gmail.com>
2014-10-17 18:16:30 -0600

    Oak Elementary (Main) All these seem to be running fine as of now.

--- oakElementary/src/oakelementary/OakElementary.java
+++ oakElementary/src/oakelementary/OakElementary.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package oakelementary;
 
-// Imports all the Class files we need
+// * Imports all the Class files we need
 import byui.cit260.oakElementary.model.*;
 import java.util.HashSet;
 import java.util.Set;
